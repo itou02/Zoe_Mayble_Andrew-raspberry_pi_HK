cf5aba0e6f69ae1fa586d61ffdd516835449ffc8
plum_0411 <mayble0411@gmail.com>
2023-02-21 09:46:25 +0800

    刪除折線圖與長條圖切換按鈕

--- src/HourChart_whitefont.tsx
+++ src/HourChart_whitefont.tsx
@@ -94,7 +94,6 @@ const LineCharts: FC = () => {
                         yAxisIndex: 'none'
                     },
                     dataView: { readOnly: false },
-                    magicType: { type: ['line', 'bar'] },
                     saveAsImage: {}
                 }
             },
